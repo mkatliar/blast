4f717334c0cbeb620ff328ce3037027595cb236c
Mikhail Katliar <cepstr@gmail.com>
2019-11-01 13:19:44 +0300

    Added noinline versions of gemm benchmarks

--- bench/GemmDynamic.cpp
+++ bench/GemmDynamic.cpp
@@ -10,8 +10,17 @@
 
 namespace blazefeo :: benchmark
 {
+    template <typename MT1, bool SO1, typename MT2, bool SO2, typename MT3, bool SO3, typename MT4, bool SO4>
+    __attribute((noinline)) static void gemm_nt_noinline(
+        Matrix<MT1, SO1> const& A, Matrix<MT2, SO2> const& B, 
+        Matrix<MT3, SO3> const& C, Matrix<MT4, SO4>& D)
+    {
+        gemm_nt(~A, ~B, ~C, ~D);
+    }
+
+
     template <typename Real>
-    static void BM_gemm_nt_dynamic(::benchmark::State& state)
+    static void BM_gemm_nt_dynamic_inline(::benchmark::State& state)
     {
         size_t const M = state.range(0);
         size_t const N = M;
@@ -38,7 +47,38 @@ namespace blazefeo :: benchmark
         state.counters["flops"] = Counter(2 * M * N * K, Counter::kIsIterationInvariantRate);
         state.counters["m"] = M;
     }
+
+
+    template <typename Real>
+    static void BM_gemm_nt_dynamic_noinline(::benchmark::State& state)
+    {
+        size_t const M = state.range(0);
+        size_t const N = M;
+        size_t const K = M;
+
+        DynamicPanelMatrix<Real> A(M, K);
+        DynamicPanelMatrix<Real> B(N, K);
+        DynamicPanelMatrix<Real> C(M, N);
+        DynamicPanelMatrix<Real> D(M, N);
+
+        randomize(A);
+        randomize(B);
+        randomize(C);
+
+        for (auto _ : state)
+        {
+            gemm_nt_noinline(A, B, C, D);
+            DoNotOptimize(A);
+            DoNotOptimize(B);
+            DoNotOptimize(C);
+            DoNotOptimize(D);
+        }
+
+        state.counters["flops"] = Counter(2 * M * N * K, Counter::kIsIterationInvariantRate);
+        state.counters["m"] = M;
+    }
     
 
-    BENCHMARK_TEMPLATE(BM_gemm_nt_dynamic, double)->DenseRange(1, 50);
+    BENCHMARK_TEMPLATE(BM_gemm_nt_dynamic_inline, double)->DenseRange(1, 50);
+    BENCHMARK_TEMPLATE(BM_gemm_nt_dynamic_noinline, double)->DenseRange(1, 50);
 }
--- bench/GemmStatic.cpp
+++ bench/GemmStatic.cpp
@@ -8,10 +8,24 @@
 #include <memory>
 
 
+#define BENCHMARK_GEMM_NT_STATIC(type, size) \
+    BENCHMARK_TEMPLATE(BM_gemm_nt_static_inline, type, size); \
+    BENCHMARK_TEMPLATE(BM_gemm_nt_static_noinline, type, size);
+
+
 namespace blazefeo :: benchmark
 {
+    template <typename MT1, bool SO1, typename MT2, bool SO2, typename MT3, bool SO3, typename MT4, bool SO4>
+    __attribute((noinline)) static void gemm_nt_noinline(
+        Matrix<MT1, SO1> const& A, Matrix<MT2, SO2> const& B, 
+        Matrix<MT3, SO3> const& C, Matrix<MT4, SO4>& D)
+    {
+        gemm_nt(~A, ~B, ~C, ~D);
+    }
+
+
     template <typename Real, size_t M>
-    static void BM_gemm_nt_static(::benchmark::State& state)
+    static void BM_gemm_nt_static_inline(::benchmark::State& state)
     {
         size_t constexpr N = M;
         size_t constexpr K = M;
@@ -39,54 +53,83 @@ namespace blazefeo :: benchmark
     }
 
 
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 1);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 2);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 3);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 4);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 5);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 6);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 7);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 8);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 9);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 10);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 11);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 12);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 13);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 14);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 15);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 16);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 17);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 18);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 19);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 20);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 21);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 22);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 23);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 24);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 25);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 26);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 27);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 28);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 29);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 30);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 31);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 32);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 33);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 34);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 35);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 36);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 37);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 38);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 39);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 40);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 41);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 42);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 43);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 44);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 45);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 46);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 47);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 48);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 49);
-    BENCHMARK_TEMPLATE(BM_gemm_nt_static, double, 50);
+    template <typename Real, size_t M>
+    static void BM_gemm_nt_static_noinline(::benchmark::State& state)
+    {
+        size_t constexpr N = M;
+        size_t constexpr K = M;
+
+        StaticPanelMatrix<Real, M, K> A;
+        StaticPanelMatrix<Real, N, K> B;
+        StaticPanelMatrix<Real, M, N> C;
+        StaticPanelMatrix<Real, M, N> D;
+
+        randomize(A);
+        randomize(B);
+        randomize(C);
+
+        for (auto _ : state)
+        {
+            gemm_nt_noinline(A, B, C, D);
+            DoNotOptimize(A);
+            DoNotOptimize(B);
+            DoNotOptimize(C);
+            DoNotOptimize(D);
+        }
+
+        state.counters["flops"] = Counter(2 * M * N * K, Counter::kIsIterationInvariantRate);
+        state.counters["m"] = M;
+    }
+
+
+    BENCHMARK_GEMM_NT_STATIC(double, 1);
+    BENCHMARK_GEMM_NT_STATIC(double, 2);
+    BENCHMARK_GEMM_NT_STATIC(double, 3);
+    BENCHMARK_GEMM_NT_STATIC(double, 4);
+    BENCHMARK_GEMM_NT_STATIC(double, 5);
+    BENCHMARK_GEMM_NT_STATIC(double, 6);
+    BENCHMARK_GEMM_NT_STATIC(double, 7);
+    BENCHMARK_GEMM_NT_STATIC(double, 8);
+    BENCHMARK_GEMM_NT_STATIC(double, 9);
+    BENCHMARK_GEMM_NT_STATIC(double, 10);
+    BENCHMARK_GEMM_NT_STATIC(double, 11);
+    BENCHMARK_GEMM_NT_STATIC(double, 12);
+    BENCHMARK_GEMM_NT_STATIC(double, 13);
+    BENCHMARK_GEMM_NT_STATIC(double, 14);
+    BENCHMARK_GEMM_NT_STATIC(double, 15);
+    BENCHMARK_GEMM_NT_STATIC(double, 16);
+    BENCHMARK_GEMM_NT_STATIC(double, 17);
+    BENCHMARK_GEMM_NT_STATIC(double, 18);
+    BENCHMARK_GEMM_NT_STATIC(double, 19);
+    BENCHMARK_GEMM_NT_STATIC(double, 20);
+    BENCHMARK_GEMM_NT_STATIC(double, 21);
+    BENCHMARK_GEMM_NT_STATIC(double, 22);
+    BENCHMARK_GEMM_NT_STATIC(double, 23);
+    BENCHMARK_GEMM_NT_STATIC(double, 24);
+    BENCHMARK_GEMM_NT_STATIC(double, 25);
+    BENCHMARK_GEMM_NT_STATIC(double, 26);
+    BENCHMARK_GEMM_NT_STATIC(double, 27);
+    BENCHMARK_GEMM_NT_STATIC(double, 28);
+    BENCHMARK_GEMM_NT_STATIC(double, 29);
+    BENCHMARK_GEMM_NT_STATIC(double, 30);
+    BENCHMARK_GEMM_NT_STATIC(double, 31);
+    BENCHMARK_GEMM_NT_STATIC(double, 32);
+    BENCHMARK_GEMM_NT_STATIC(double, 33);
+    BENCHMARK_GEMM_NT_STATIC(double, 34);
+    BENCHMARK_GEMM_NT_STATIC(double, 35);
+    BENCHMARK_GEMM_NT_STATIC(double, 36);
+    BENCHMARK_GEMM_NT_STATIC(double, 37);
+    BENCHMARK_GEMM_NT_STATIC(double, 38);
+    BENCHMARK_GEMM_NT_STATIC(double, 39);
+    BENCHMARK_GEMM_NT_STATIC(double, 40);
+    BENCHMARK_GEMM_NT_STATIC(double, 41);
+    BENCHMARK_GEMM_NT_STATIC(double, 42);
+    BENCHMARK_GEMM_NT_STATIC(double, 43);
+    BENCHMARK_GEMM_NT_STATIC(double, 44);
+    BENCHMARK_GEMM_NT_STATIC(double, 45);
+    BENCHMARK_GEMM_NT_STATIC(double, 46);
+    BENCHMARK_GEMM_NT_STATIC(double, 47);
+    BENCHMARK_GEMM_NT_STATIC(double, 48);
+    BENCHMARK_GEMM_NT_STATIC(double, 49);
+    BENCHMARK_GEMM_NT_STATIC(double, 50);
 }
