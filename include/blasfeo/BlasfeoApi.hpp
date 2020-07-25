8fbba321028aaff83a08666fefe719d01c001a95
Mikhail Katliar <cepstr@gmail.com>
2020-07-25 09:59:42 +0300

    * Wrapped more blasfeo API functions * Added BlazeInterop.hpp

--- include/blasfeo/BlasfeoApi.hpp
+++ include/blasfeo/BlasfeoApi.hpp
@@ -39,12 +39,12 @@ namespace blasfeo
     }
 
 
-    // @brief returns the memory size (in bytes) needed for a BLASFEO matrix data with elements of type Real
+    /// @brief returns the memory size (in bytes) needed for a BLASFEO matrix data with elements of type Real
     template <typename Real>
     size_t memsize_mat(size_t m, size_t n);
 
 
-    // @brief returns the memory size (in bytes) needed for a double-precision BLASFEO matrix data
+    /// @brief returns the memory size (in bytes) needed for a double-precision BLASFEO matrix data
     template <>
     inline size_t memsize_mat<double>(size_t m, size_t n)
     {
@@ -52,7 +52,7 @@ namespace blasfeo
     }
 
 
-    // @brief returns the memory size (in bytes) needed for a double-precision BLASFEO matrix data
+    /// @brief returns the memory size (in bytes) needed for a double-precision BLASFEO matrix data
     template <>
     inline size_t memsize_mat<float>(size_t m, size_t n)
     {
@@ -102,30 +102,113 @@ namespace blasfeo
     }
 
 
-    // @brief Pack the column-major double-precision matrix A into BLASFEO matrix B
+    /// @brief Pack the column-major double-precision matrix A into BLASFEO matrix B
     inline void pack_mat(size_t m, size_t n, double const * A, size_t lda, blasfeo_dmat& sB, size_t bi, size_t bj)
     {
         blasfeo_pack_dmat(m, n, const_cast<double *>(A), lda, &sB, bi, bj);
     }
 
 
-    // @brief Pack the column-major single-precision matrix A into BLASFEO matrix B
+    /// @brief Pack the column-major single-precision matrix A into BLASFEO matrix B
     inline void pack_mat(size_t m, size_t n, float const * A, size_t lda, blasfeo_smat& sB, size_t bi, size_t bj)
     {
         blasfeo_pack_smat(m, n, const_cast<float *>(A), lda, &sB, bi, bj);
     }
 
 
-    // @brief Unpack the column-major double-precision BLASFEO matrix A into the column-major matrix B
+    /// @brief Transpose and pack the column-major matrix A into the matrix struct B
+    inline void pack_tran_mat(size_t m, size_t n, double const * A, size_t lda, blasfeo_dmat& sB, size_t bi, size_t bj)
+    {
+        ::blasfeo_pack_tran_dmat(m, n, const_cast<double *>(A), lda, &sB, bi, bj);
+    }
+
+
+    /// @brief Transpose and pack the column-major matrix A into the matrix struct B
+    inline void pack_tran_mat(size_t m, size_t n, float const * A, size_t lda, blasfeo_smat& sB, size_t bi, size_t bj)
+    {
+        ::blasfeo_pack_tran_smat(m, n, const_cast<float *>(A), lda, &sB, bi, bj);
+    }
+
+
+    /// @brief Unpack the column-major double-precision BLASFEO matrix A into the column-major matrix B
     inline void unpack_mat(size_t m, size_t n, blasfeo_dmat& sA, size_t ai, size_t aj, double * B, size_t ldb)
     {
         blasfeo_unpack_dmat(m, n, &sA, ai, aj, B, ldb);
     }
 
     
-    // @brief Unpack the column-major single-precision BLASFEO matrix A into the column-major matrix B
+    /// @brief Unpack the column-major single-precision BLASFEO matrix A into the column-major matrix B
     inline void unpack_mat(size_t m, size_t n, blasfeo_smat& sA, size_t ai, size_t aj, float * B, size_t ldb)
     {
         blasfeo_unpack_smat(m, n, &sA, ai, aj, B, ldb);
+    }
+
+
+    /// @brief Transpose and unpack the matrix structure A into the column-major matrix B
+    inline void unpack_tran_mat(size_t m, size_t n, blasfeo_dmat const& sA, size_t ai, size_t aj, double * B, size_t ldb)
+    {
+        ::blasfeo_unpack_tran_dmat(m, n, const_cast<blasfeo_dmat *>(&sA), ai, aj, B, ldb);
+    }
+
+
+    /// @brief Transpose and unpack the matrix structure A into the column-major matrix B
+    inline void unpack_tran_mat(size_t m, size_t n, blasfeo_smat const& sA, size_t ai, size_t aj, float * B, size_t ldb)
+    {
+        ::blasfeo_unpack_tran_smat(m, n, const_cast<blasfeo_smat *>(&sA), ai, aj, B, ldb);
+    }
+
+
+    /// @brief Pack the vector x into the vector structure y
+    inline void pack_vec(size_t m, double const * x, blasfeo_dvec& sy, size_t yi)
+    {
+        ::blasfeo_pack_dvec(m, const_cast<double *>(x), &sy, yi);
+    }
+
+
+    /// @brief Pack the vector x into the vector structure y
+    inline void pack_vec(size_t m, float const * x, blasfeo_svec& sy, size_t yi)
+    {
+        ::blasfeo_pack_svec(m, const_cast<float *>(x), &sy, yi);
+    }
+
+
+    /// @brief Unpack the vector structure x into the vector y
+    inline void unpack_vec(size_t m, blasfeo_dvec const& sx, size_t xi, double * y)
+    {
+        ::blasfeo_unpack_dvec(m, const_cast<blasfeo_dvec *>(&sx), xi, y);
+    }
+
+
+    /// @brief Unpack the vector structure x into the vector y
+    inline void unpack_vec(size_t m, blasfeo_svec const& sx, size_t xi, float * y)
+    {
+        ::blasfeo_unpack_svec(m, const_cast<blasfeo_svec *>(&sx), xi, y);
+    }
+
+
+    /// @brief a <= alpha
+    inline void vecse(size_t m, double alpha, blasfeo_dvec& sx, size_t xi)
+    {
+        ::blasfeo_dvecse(m, alpha, &sx, xi);
+    }
+
+
+    /// @brief a <= alpha
+    inline void vecse(size_t m, float alpha, blasfeo_svec& sx, size_t xi)
+    {
+        ::blasfeo_svecse(m, alpha, &sx, xi);
+    }
+
+
+    /// @brief x <= alpha*x
+    inline void vecsc(size_t m, double alpha, blasfeo_dvec& sx, size_t xi)
+    {
+        ::blasfeo_dvecsc(m, alpha, &sx, xi);
+    }
+
+    /// @brief x <= alpha*x
+    inline void vecsc(size_t m, float alpha, blasfeo_svec& sx, size_t xi)
+    {
+        ::blasfeo_svecsc(m, alpha, &sx, xi);
     }
 }
