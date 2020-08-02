f3e84ab3686234285ef8991e2e2d645961bf020e
Mikhail Katliar <cepstr@gmail.com>
2020-08-02 17:52:48 +0300

    Implemented trsvLeftUpper()

--- include/blazefeo/math/dense/Trsv.hpp
+++ include/blazefeo/math/dense/Trsv.hpp
@@ -24,12 +24,41 @@ namespace blazefeo
 
         for (size_t i = 0; i < N; ++i)
         { 
-           (~x)[i] = (~b)[i]; 
+           auto acc = (~b)[i]; 
 
            for (size_t j = 0; j < i; ++j)
-                (~x)[i] -= (~A)(i, j) * (~x)[j];
+                acc -= (~A)(i, j) * (~x)[j];
 
-           (~x)[i] *= inv((~A)(i, i));
+           (~x)[i] = acc / (~A)(i, i);
+        } 
+    }
+
+
+    /// @brief Solve A*x = b, A upper triangular.
+    ///
+    template <
+        typename MT, bool SO,
+        typename VT1, bool TF1,
+        typename VT2, bool TF2
+    >
+    inline void trsvLeftUpper(DenseMatrix<MT, SO> const& A,
+        DenseVector<VT1, TF1> const& b, DenseVector<VT2, TF2>& x)
+    {
+        size_t const N = size(b);
+
+        if (rows(A) != N || columns(A) != N)
+            BLAZE_THROW_INVALID_ARGUMENT("Invalid argument size");
+
+        resize(x, N);
+
+        for (size_t i = N; i-- > 0; )
+        { 
+           auto acc = (~b)[i]; 
+
+           for (size_t j = i + 1; j < N; ++j)
+                acc -= (~A)(i, j) * (~x)[j];
+
+           (~x)[i] = acc / (~A)(i, i);
         } 
     }
 }
--- test/math/dense/TrsvTest.cpp
+++ test/math/dense/TrsvTest.cpp
@@ -24,4 +24,25 @@ namespace blazefeo :: testing
                 << "trsv error at size n=" << n;
         }
     }
+
+
+    TEST(DenseTrsvTest, testLeftUpper)
+    {
+        for (size_t n = 1; n <= 20; ++n)
+        {
+            // Init Blaze matrices
+            //
+            UpperMatrix<DynamicMatrix<double>> A(n, n);
+            DynamicVector<double> b(n);
+            DynamicVector<double> x(n);
+            randomize(A);
+            randomize(b);
+
+            // Do trsv
+            trsvLeftUpper(A, b, x);
+
+            BLAZEFEO_ASSERT_APPROX_EQ(evaluate(A * x), b, 1e-10, 1e-10)
+                << "trsv error at size n=" << n;
+        }
+    }
 }
