24b9365cc19082dc45a74f9abbe6561cb3da6f64
Mikhail Katliar <cepstr@gmail.com>
2020-09-03 18:09:06 +0300

    Added license

--- include/blasfeo/blasfeo_api/Vec.hpp
+++ include/blasfeo/blasfeo_api/Vec.hpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2019-2020 Mikhail Katliar All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 #pragma once
 
 #include <blasfeo_d_aux.h>
