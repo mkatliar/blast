99a4e28964e2d06fa3e0a1d0e5c6307c1a7c16eb
Mikhail Katliar <mikhail.katliar@protonmail.com>
2024-08-08 09:36:34 +0200

    Operations on general matrices: randomize(), operator==(), assign(), operator<<()

--- include/blast/math/Matrix.hpp
+++ include/blast/math/Matrix.hpp
@@ -9,7 +9,12 @@
 #include <blast/math/dense/StaticMatrixPointer.hpp>
 #include <blast/math/panel/DynamicPanelMatrixPointer.hpp>
 #include <blast/math/panel/StaticPanelMatrixPointer.hpp>
+#include <blast/util/Types.hpp>
 #include <blast/system/Inline.hpp>
+
+#include <iosfwd>
+#include <stdexcept>
+#include <random>
 
 
 namespace blast
@@ -43,5 +48,66 @@ namespace blast
     BLAST_ALWAYS_INLINE auto ptr(MT const& m)
     {
         return ptr<IsAligned_v<MT>>(m, 0, 0);
+    }
+	
+	
+    template <Matrix M>
+    inline void randomize(M& m) noexcept
+    {
+        std::mt19937 rng;
+        std::uniform_real_distribution<ElementType_t<M>> dist;
+
+        for (size_t i = 0; i < rows(m); ++i)
+            for (size_t j = 0; j < columns(m); ++j)
+                m(i, j) = dist(rng);
+    }
+
+
+    template <Matrix MA, Matrix MB>
+    inline bool operator==(MA const& a, MB const& b)
+    {
+        size_t const M = rows(a);
+        size_t const N = columns(a);
+
+        if (M != rows(b) || N != columns(b))
+            throw std::invalid_argument {"Inconsistent matrix sizes"};
+
+        for (size_t i = 0; i < M; ++i)
+            for (size_t j = 0; j < N; ++j)
+                if (a(i, j) != b(i, j))
+                    return false;
+
+        return true;
+    }
+
+
+    template <Matrix MA, Matrix MB>
+    inline MA& assign(MA& a, MB const& b)
+    {
+        size_t const M = rows(a);
+        size_t const N = columns(a);
+
+        if (M != rows(b) || N != columns(b))
+            throw std::invalid_argument {"Inconsistent matrix sizes"};
+
+        for (size_t i = 0; i < M; ++i)
+            for (size_t j = 0; j < N; ++j)
+                a(i, j) = b(i, j);
+
+        return a;
+    }
+
+
+    template <Matrix M>
+    inline std::ostream& operator<<(std::ostream& os, M const& m)
+    {
+        for (size_t i = 0; i < rows(m); ++i)
+        {
+            for (size_t j = 0; j < columns(m); ++j)
+                os << m(i, j) << "\t";
+            os << std::endl;
+        }
+
+        return os;
     }
 }
--- include/blast/math/typetraits/IsDenseMatrix.hpp
+++ include/blast/math/typetraits/IsDenseMatrix.hpp
@@ -6,6 +6,7 @@
 
 #include <type_traits>
 
+
 namespace blast
 {
     /**
