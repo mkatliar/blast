737200299295c091cc828ca31bb4b6cf3612add7
Robin Verschueren <robin.verschueren@pm.me>
2024-10-09 16:22:10 +0200

    Make benchmarks great again

--- bench/analysis/dgemm_performance.py
+++ bench/analysis/dgemm_performance.py
@@ -2,8 +2,6 @@ import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import json
-import glob
-import pathlib
 
 def filter_aggregate(benchmarks, name):
     result = []
@@ -16,29 +14,30 @@ def filter_aggregate(benchmarks, name):
     return result
 
 
+def load_benchmark(file_name):
+    with open(file_name) as f:
+        return filter_aggregate(json.load(f)['benchmarks'], 'median')
+
+
 factor = 1e+9 # Giga
 style = 'o-'
 line_width = 1
 
-# prop_cycle = plt.rcParams['axes.prop_cycle']
-# c = prop_cycle.by_key()['color']
-
 plots = [
-    # {'data_file': 'dgemm-openblas.json', 'label': 'OpenBLAS'},
-    # {'data_file': 'dgemm-mkl.json', 'label': 'MKL'},
+    {'data_file': 'dgemm-openblas.json', 'label': 'OpenBLAS'},
+    {'data_file': 'dgemm-mkl.json', 'label': 'MKL'},
     {'data_file': 'dgemm-blasfeo.json', 'label': 'BLASFEO'},
     # {'data_file': 'dgemm-blasfeo-blas.json', 'label': 'BLASFEO*'},
-    # {'data_file': 'dgemm-libxsmm.json', 'label': 'LIBXSMM'},
+    {'data_file': 'dgemm-libxsmm.json', 'label': 'LIBXSMM'},
     # {'data_file': 'dgemm-eigen-dynamic.json', 'label': 'Eigen (D)'},
-    # {'data_file': 'dgemm-eigen-static.json', 'label': 'Eigen (S)'},
+    {'data_file': 'dgemm-eigen-static.json', 'label': 'Eigen (S)'},
     # {'data_file': 'dgemm-blaze-dynamic.json', 'label': 'Blaze (D)'},
     # {'data_file': 'dgemm-blaze-static.json', 'label': 'Blaze (S)'},
+    {'data_file': 'dgemm-blast-static-panel.json', 'label': 'StaPan'},
+    {'data_file': 'dgemm-blast-static-plain.json', 'label': 'StaPla'},
+    {'data_file': 'dgemm-blast-dynamic-panel.json', 'label': 'DynPan'},
+    {'data_file': 'dgemm-blast-dynamic-plain.json', 'label': 'DynPla'},
 ]
-
-for benchmark_file, benchmark_label in [('dgemm-blast-static-panel.json', 'SP'), ('dgemm-blast-static-plain.json', 'SD'), ('dgemm-blast-dynamic-panel.json', 'DP'), ('dgemm-blast-dynamic-plain.json', 'DD')]:
-    files = glob.glob('./**/' + benchmark_file, recursive=True, root_dir='bench_result/data')
-    for file in files:
-        plots.append({'data_file': file, 'label': f'BLAST ({benchmark_label}) {pathlib.Path(file).parent.stem}'})
 
 fig = plt.figure(figsize=[10, 6])
 ax = fig.subplots()
